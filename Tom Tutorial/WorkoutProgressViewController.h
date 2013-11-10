fd96f35032ddb2e964d52d39e40944e3a63fe1d5
Jen Liu <jkliu@mit.edu>
2013-11-10 14:50:57 -0500

    add WorkoutProgressViewController

---
+++ Tom Tutorial/WorkoutProgressViewController.h
@@ -0,0 +1,13 @@
+//
+//  WorkoutProgressViewController.h
+//  Tom Tutorial
+//
+//  Created by Jen Liu on 11/10/13.
+//  Copyright (c) 2013 MIT PPAT Team Jonathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WorkoutProgressViewController : UIViewController
+
+@end
