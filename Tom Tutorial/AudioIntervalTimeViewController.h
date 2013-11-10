626ca5a7759eb7813c29a39900f0f8d6b1f35417
Joe Henke <jdhenke@mit.edu>
2013-11-10 14:19:10 -0500

    10 nov sync

---
+++ Tom Tutorial/AudioIntervalTimeViewController.h
@@ -0,0 +1,13 @@
+//
+//  AudioIntervalTimeViewController.h
+//  Tom Tutorial
+//
+//  Created by Joseph Henke on 11/10/13.
+//  Copyright (c) 2013 MIT PPAT Team Jonathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AudioIntervalTimeViewController : UIViewController
+
+@end
---
+++ Tom Tutorial/AudioIntervalTimeViewController.m
@@ -0,0 +1,38 @@
+//
+//  AudioIntervalTimeViewController.m
+//  Tom Tutorial
+//
+//  Created by Joseph Henke on 11/10/13.
+//  Copyright (c) 2013 MIT PPAT Team Jonathan. All rights reserved.
+//
+
+#import "AudioIntervalTimeViewController.h"
+
+@interface AudioIntervalTimeViewController ()
+
+@end
+
+@implementation AudioIntervalTimeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
