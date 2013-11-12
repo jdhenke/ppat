2728c019fc44a792b2c73d1feaa75625d87ad39b
Jen Liu <jkliu@mit.edu>
2013-11-12 15:40:39 -0500

    add WorkoutSummaryViewController

---
+++ WorkoutSummaryViewController.h
@@ -0,0 +1,18 @@
+//
+//  WorkoutSummaryViewController.h
+//  Tom Tutorial
+//
+//  Created by Jen Liu on 11/12/13.
+//  Copyright (c) 2013 MIT PPAT Team Jonathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class Workout;
+
+@interface WorkoutSummaryViewController : UIViewController
+
+// once the workout is saved it will go in this variable
+// @property(nonatomic, retain) Workout *workout;
+
+@end
---
+++ WorkoutSummaryViewController.m
@@ -0,0 +1,38 @@
+//
+//  WorkoutSummaryViewController.m
+//  Tom Tutorial
+//
+//  Created by Jen Liu on 11/12/13.
+//  Copyright (c) 2013 MIT PPAT Team Jonathan. All rights reserved.
+//
+
+#import "WorkoutSummaryViewController.h"
+
+@interface WorkoutSummaryViewController ()
+
+@end
+
+@implementation WorkoutSummaryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
